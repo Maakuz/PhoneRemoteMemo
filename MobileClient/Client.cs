40df8b83066de1dbfec03f2f0edd18ff5699c6f4
Markus FAST HEMMA <markuswerdien@hotmail.com>
2019-12-27 10:33:29 +0100

    Add project files.

---
+++ MobileClient/Client.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+public class Client
+{
+    private int m_port;
+    private string m_ip;
+
+    public Client(int port, string ip)
+    {
+        m_port = port;
+        m_ip = ip;
+    }
+
+    public bool testConnection()
+    {
+        bool connected = false;
+        TcpClient client = new TcpClient(m_ip, m_port);
+        if (client.Connected)
+            connected = true;
+
+        client.Close();
+
+        return connected;
+    }
+
+    public string sendMessage(string message)
+    {
+        if (message.Length == 0)
+            return "No meesage to send";
+
+        TcpClient client = new TcpClient(m_ip, m_port);
+        NetworkStream netStream = client.GetStream();
+
+        byte[] bytes = ASCIIEncoding.ASCII.GetBytes(message);
+
+        netStream.Write(bytes, 0, bytes.Length);
+
+        byte[] recievedBytes = new byte[client.ReceiveBufferSize];
+        int bytesRead = netStream.Read(recievedBytes, 0, client.ReceiveBufferSize);
+
+
+        return Encoding.ASCII.GetString(recievedBytes, 0, bytesRead);
+    }
+}
---
+++ MobileClient/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android;
+using System.Collections.Generic;
+
+namespace MobileClient
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Client m_client;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Get our UI controls from the loaded layout
+            EditText message = FindViewById<EditText>(Resource.Id.input_text);
+            TextView recieved = FindViewById<TextView>(Resource.Id.text_recieved);
+            Button sendbutton = FindViewById<Button>(Resource.Id.btn_send);
+            Button updateButton = FindViewById<Button>(Resource.Id.btn_update);
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner_ips);
+
+            updateIPs(spinner);
+
+            updateButton.Click += (sender, e) =>
+            {
+                updateIPs(spinner);
+            };
+            
+
+            sendbutton.Click += (sender, e) =>
+            {
+                m_client = new Client(5000, spinner.SelectedItem.ToString());
+
+                string recievedMessage = m_client.sendMessage(message.Text);
+                if (string.IsNullOrWhiteSpace(recievedMessage))
+                {
+                    recieved.Text = string.Empty;
+                }
+                else
+                {
+                    recieved.Text = recievedMessage;
+                }
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void updateIPs(Spinner spinner)
+        {
+            string[] ips = { "192.168.43.171"};
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, ips);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+        }
+    }
+}
